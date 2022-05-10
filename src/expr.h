d62ba6c330f571db13a0a2b439c2ed7bb050d26c
Rodney McCoy <66753299+RodneyMcCoy@users.noreply.github.com>
2022-05-10 13:26:34 -0700

    Update expr.h

--- src/expr.h
+++ src/expr.h
@@ -1,10 +1,38 @@
+/* expr.h
+ *
+ * Rodney McCoy 
+ * rbmj2001@outlook.com
+ * https://github.com/RodneyMcCoy/expression-tree/edit/main/src/expr.h
+ *
+ * Expression Tree Class Interface.
+ */
+
+
 class expr {
 private:
-  
-public:
-  initString(char *);
-  initPolynomial(double *);
-  initFourier(double *);
-  initRandom();
+   typedef struct node {
+      int id;
+      node * left;
+      node * right;
+   }
+   node * root;
 
+   map operands;
+   map leafs;
+   char * label;
+public:
+   expr();
+   ~expr();
+   expr operator=(const expr&);
+  
+   void initString(char * str);
+   void initPolynomial(double * consts);
+   void initFourier(double * consts);
+   void initRandom(int size); // -1 for random size
+
+   assign(char * label, double val);
+   getIndex(char * label);
+   assign(int label, double val);
+  
+   evaluate();
 };
