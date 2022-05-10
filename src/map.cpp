9b93ed76525c09372fc3cd30843c4cab8cbad968
Rodney McCoy <66753299+RodneyMcCoy@users.noreply.github.com>
2022-05-10 13:27:59 -0700

    Update map.cpp

--- src/map.cpp
+++ src/map.cpp
@@ -1,1 +1,8 @@
-// map.cpp
+/* map.h
+ *
+ * Rodney McCoy 
+ * rbmj2001@outlook.com
+ * https://github.com/RodneyMcCoy/expression-tree/edit/main/src/expr.h
+ *
+ * Implimentation for a class that maps strings to integers and vice versa.
+ */
