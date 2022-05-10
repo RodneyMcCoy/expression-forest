1db7ec70119c92591d28ddac1f5db2c284a6e5b2
Rodney McCoy <66753299+RodneyMcCoy@users.noreply.github.com>
2022-05-10 13:26:50 -0700

    Update expr.cpp

--- src/expr.cpp
+++ src/expr.cpp
@@ -1,1 +1,8 @@
-// expr.cpp
+/* expr.cpp
+ *
+ * Rodney McCoy 
+ * rbmj2001@outlook.com
+ * https://github.com/RodneyMcCoy/expression-tree/edit/main/src/expr.h
+ *
+ * Expression Tree Class Implimentation.
+ */
