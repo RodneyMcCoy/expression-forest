ea004bbfb6458bc73869b77ffda1b80a68ee82a8
Rodney McCoy <66753299+RodneyMcCoy@users.noreply.github.com>
2022-05-10 14:18:41 -0700

    Update expr.h

--- src/expr.h
+++ src/expr.h
@@ -31,23 +31,35 @@ public:
 
 
 // Initializers
-   bool initString(char * str);                    // pases string to lex / yacc parser, returns false if format not valid
-   void initPolynomial(double * consts, int n);    // initalizes a degree n-1th polynomial with given constants
-   void initFourier(double * consts, int n);       // initalizes a fourier series with 2n + 1 terms
-   void initRandom(int size=-1);                   // randomly fill tree
-   void initSeries(const expr& x, int n);                 // expr x is an expression tree with one variable. 
-   void initProduct(const expr& x, int n);                
-
+   bool initString(char * str);                 // pases string to lex / yacc parser, returns false if format not valid
+   void initPolynomial(double * consts, int n); // initalizes a degree nth polynomial with given constants
+   void initFourier(double * consts, int n);    // initalizes a fourier series with  terms
+   void initRandom(int size=-1);                // randomly fill tree
    
+
+// Initialziers Part 2
+// a_i must be a tree with only varaibles i and x.
+// i being the index which turns into a constant, x being an evaluatable varaible
+// initSeries(a_i, 0, n) with a_i = x^n would be equivalent to initPolynomial({1, ..., 1}, n)
+   bool initSeries(const expr& a_i, int n);                 // creates a n+1 termed series using a_i          
+   bool initProduct(const expr& a_i, int n);                // creates a n+1 termed product using a_i
+   bool initWithOperand(const expr& a_i, int opInd, int l, int u); // creates a n termed product 
+   bool initNested(const expr&, int constInd, int opInd, int n); 
+   
+// Call private map methods to get indicies
+   int getLeafIndex(char * label);
+   int getOperandIndex(char * label);
+
+
 // Assigns values to leaves in tree
-   bool assign(char * label, double val);    // passes label through map, and gives
-   int getIndex(char * label);
-   bool assign(int label, double val);
+   bool assign(char * label, double val);    // Assigns the leaf label the value val
+   bool assign(int label, double val);       // Assings the leaf with index label the value val
 
    
 // DFS the tree and evalute given saved constants in the map
    double evaluate();
    
+   
 // Check format of expr tree
-   bool properFormat(char * label, char ** consts); // throws error if label doesnt match trees label, or a single string in consts isnt contained in the map
+   bool properFormat(char * label, char ** consts); // returns false if label doesnt match trees label, or a single string in consts isn't contained in the map
 };
