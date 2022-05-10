55dc01de8eac9351b8ba0c30408d57e734735f3b
Rodney McCoy <66753299+RodneyMcCoy@users.noreply.github.com>
2022-05-10 13:27:37 -0700

    Update map.h

--- src/map.h
+++ src/map.h
@@ -1,1 +1,8 @@
-// map.h
+/* map.h
+ *
+ * Rodney McCoy 
+ * rbmj2001@outlook.com
+ * https://github.com/RodneyMcCoy/expression-tree/edit/main/src/expr.h
+ *
+ * Interface for a class that maps strings to integers and vice versa.
+ */
