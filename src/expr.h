3ec628bd7d0425fc7977dd3785ed94edec4cd4ab
Rodney McCoy <66753299+RodneyMcCoy@users.noreply.github.com>
2022-05-10 19:55:08 -0700

    Update expr.h

--- src/expr.h
+++ src/expr.h
@@ -23,6 +23,7 @@ private:
    double * leafs;
    
    char * treeLabel;
+
 public:
 // Constructors, Destructors, and Overloaded Operators
    expr(char * str = NULL);      // construct tree. If arg given, calls initString
@@ -35,18 +36,9 @@ public:
    void initPolynomial(double * consts, int degree);  // initalizes a polynomial with given constants. consts of length degree + 1.
    void initFourier(double * consts, int len);        // initalizes a fourier series with len terms
    void initRandom(int size=-1);                      // randomly fill tree with operands and constants. If size >= 1, size is max amount of vertices in tree.
+   bool initSeries(const expr& a_i, int l, int u);    // creates a n+1 termed series using a_i          
+   bool initProduct(const expr& a_i, int l, int u);   // creates a n+1 termed product using a_i
    
-
-// Initialziers Part 2
-/* 
- * a_i must be a tree with only varaibles i and x.
- * i being the index which turns into a constant, x being an evaluatable varaible
- * initSeries(a_i, 0, n) with a_i = x^n would be equivalent to initPolynomial({1, ..., 1}, n)
- */
-   bool initSeries(const expr& a_i, int l, int u);                // creates a n+1 termed series using a_i          
-   bool initProduct(const expr& a_i, int l, int u);               // creates a n+1 termed product using a_i
-   bool initWithOperand(const expr& a_i, int opInd, int l, int u);// creates a n termed product 
-   bool initNested(const expr&, int constInd, int opInd, int n); 
    
 // Call private map methods to get indicies
    int getLeafIndex(char * label);
@@ -63,5 +55,10 @@ public:
    
    
 // Check format of expr tree
-   bool properFormat(char * label, char ** consts); // returns false if label doesnt match trees label, or a single string in consts isn't contained in the map
+   bool isProperFormat(char * label, char ** consts); // returns false if label doesnt match trees label, or a single string in consts isn't contained in the map
+   
+  
+// Optimize
+   expr & optimizeOther();
+   void optimizeSelf();
 };
