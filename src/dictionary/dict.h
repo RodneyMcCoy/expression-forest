a71dc7f997a8bbad6ab475083d3075c6cb9000bd
Rodney McCoy <66753299+RodneyMcCoy@users.noreply.github.com>
2022-05-14 05:37:04 -0700

    Update dict.h

--- src/dictionary/dict.h
+++ src/dictionary/dict.h
@@ -10,5 +10,39 @@
 #ifndef DICT_H
 #define DICT_H
 
+template<> struct type_name<int> { static int type() {return -1;} };
+template<> struct type_name<string> { static int type() {return 1;} };
+
+template <typename Data>
+class dict {
+private:
+  // This is an AVL tree
+  typedef struct node {
+    char * label;
+    unsigned short int id;
+    node * left;
+    node * right;
+  }
+  node * root;
+   
+  Data * vals=NULL;
+
+  a
+
+public:
+// Constructors, Destructors, and other Overloaded Operators
+  dict();
+  ~dict();
+  dict operator=(const dict&);  // assignment (=), copys one dictionary to another
+
+  bool insert(char * label);
+  bool remove(char * label);
+  
+  Data lookup(char * label);
+  Data operator[](char * label);
+  Data lookup(int id);
+  Data operator[](int id);
+};
+
 
 #endif
