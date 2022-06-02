63169bb703d4efa11d069a1e37ab606376218956
RodneyMcCoy <rbmj2001@outlook.com>
2022-06-02 10:37:55 -0700

    a

--- tests/dictTest.cpp
+++ tests/dictTest.cpp
@@ -13,7 +13,7 @@
 #include "dict.h"
 
 
-char * buf[] = 
+const char * buf[] = 
 {
 	"a",  "b",  "c",  "d",  "e",  "f",  "g",  "h",  "i",  "j",  "k",  "l",  "m", 
 	"n",  "o",  "p",  "q",  "r",  "s",  "t",  "u",  "v",  "w",  "x",  "y",  "z", 
--- tests/symTest.cpp
+++ tests/symTest.cpp
@@ -11,7 +11,7 @@
 #include "sym_table.h"
 
 
-char * buf[] = 
+const char * buf[] = 
 {
 	"a",  "b",  "c",  "d",  "e",  "f",  "g",  "h",  "i",  "j",  "k",  "l",  "m", 
 	"n",  "o",  "p",  "q",  "r",  "s",  "t",  "u",  "v",  "w",  "x",  "y",  "z", 
