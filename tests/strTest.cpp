f3b0f185b6e126da9b0161bc770d3a6eacba17d7
RodneyMcCoy <rbmj2001@outlook.com>
2022-06-03 13:11:51 -0700

    Update strTest.cpp

--- tests/strTest.cpp
+++ tests/strTest.cpp
@@ -12,19 +12,19 @@
 #include "str.h"
 
 
-const char * buf[] = 
+/*const char * buf[] = 
 {
 	"a",  "b",  "c",  "d",  "e",  "f",  "g",  "h",  "i",  "j",  "k",  "l",  "m", 
 	"n",  "o",  "p",  "q",  "r",  "s",  "t",  "u",  "v",  "w",  "x",  "y",  "z", 
 	"aa", "bb", "cc", "dd", "ee", "ff", "gg", "hh", "ii", "jj", "kk", "ll", "mm", 
 	"nn", "oo", "pp", "qq", "rr", "ss", "tt", "uu", "vv", "ww", "xx", "yy", "zz"
-};
+};*/
 	
 int main() {
 	/* ----- String Tests ----- */
 	str s1;
 	
-	std::cout << "process started\n";
+	std::cout << "abcdefgcowabunga\n";
 
 	
 	const char * ch = "hello";
